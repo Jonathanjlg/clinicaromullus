76e3141d9a227f0d6e038c236f4aeba962bfc95f
Aluno <aluno@edu.senac.br>
2023-11-14 21:30:18 -0300

    clinica

---
+++ Models/Entities/Medico.cs
@@ -0,0 +1,14 @@
+public class Medico{// Inicio.
+
+   /* 
+     Variável: é um espaço reservado na
+      mémoria do computador.  
+   */
+   // POO( programação orientada a objetos.)
+   //Na POO Variável é chamada também de atributo.
+   //No C# ela pode ser um propriedade automática.
+   int Id;
+   string? CRM;
+   string Nome = string.Empty;
+   
+}//Fim.
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Clinicaromullus.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
